8e2f5b10bfd1bdce72a902278015c49514028f14
Arthur Elihimas <ArthurElihimas@lucidmotors.com>
2021-05-30 14:00:22 -0300

    Refactored SlideAnimation

--- app/src/main/java/com/elihimas/orchestra/animations/SlideAnimation.kt
+++ app/src/main/java/com/elihimas/orchestra/animations/SlideAnimation.kt
@@ -1,11 +1,114 @@
 package com.elihimas.orchestra.animations
 
-import android.animation.ValueAnimator
 import android.graphics.Rect
 import android.view.View
-import androidx.core.animation.doOnEnd
 
-//TODO add strategy to simplify code5
+interface SlideStrategy {
+    fun init(vararg views: View)
+    fun update(view: View, proportion: Float)
+}
+
+abstract class HorizontalSlideStrategy : SlideStrategy {
+
+    var initialTranslationX = 0f
+
+    override fun init(vararg views: View) {
+        initialTranslationX = views[0].translationX
+    }
+}
+
+abstract class VerticalSlideStrategy : SlideStrategy {
+
+    var initialTranslationY = 0f
+
+    override fun init(vararg views: View) {
+        initialTranslationY = views[0].translationY
+    }
+}
+
+class SlideUpReverseStrategy : VerticalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val down = view.height * proportion
+
+        view.clipBounds = Rect(0, 0, view.width, (view.height - down).toInt())
+        view.translationY = down + initialTranslationY
+    }
+}
+
+class SlideDownReverseStrategy : VerticalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val top = view.height * proportion
+
+        view.clipBounds = Rect(0, top.toInt(), view.width, view.height)
+        view.translationY = -top + initialTranslationY
+    }
+}
+
+class SlideLeftReverseStrategy : HorizontalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val right = view.width * proportion
+
+        view.clipBounds = Rect(0, 0, (view.width - right).toInt(), view.height)
+        view.translationX = right + initialTranslationX
+    }
+}
+
+class SlideRightReverseStrategy : HorizontalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val left = view.width * proportion
+
+        view.clipBounds = Rect(left.toInt(), 0, view.width, view.height)
+        view.translationX = -left + initialTranslationX
+    }
+}
+
+class SlideUpStrategy : VerticalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val down = view.height * proportion
+
+        view.clipBounds = Rect(0, 0, view.width, down.toInt())
+        view.translationY = view.height - down + initialTranslationY
+    }
+
+}
+
+class SlideDownStrategy : VerticalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val top = view.height * proportion
+
+        view.clipBounds = Rect(0, view.height - top.toInt(), view.width, view.height)
+        view.translationY = -view.height + top + initialTranslationY
+    }
+
+}
+
+class SlideLeftStrategy : HorizontalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val right = view.width * proportion
+
+        view.clipBounds = Rect(0, 0, right.toInt(), view.height)
+        view.translationX = view.width - right + initialTranslationX
+    }
+
+}
+
+class SlideRightStrategy : HorizontalSlideStrategy() {
+
+    override fun update(view: View, proportion: Float) {
+        val left = view.width * proportion
+
+        view.clipBounds = Rect(view.width - left.toInt(), 0, view.width, view.height)
+        view.translationX = -view.width + left + initialTranslationX
+    }
+}
+
 /**
  * Slide in and out animation implementation
  *
@@ -14,9 +117,7 @@ import androidx.core.animation.doOnEnd
  */
 open class SlideAnimation(var direction: Direction, private val reverseAnimation: Boolean = false) : Animation() {
 
-    val update: (view: View, proportion: Float) -> Unit
-    var initialTranslationX = 0f
-    var initialTranslationY = 0f
+    private val slideStrategy: SlideStrategy
 
     init {
         val actualDirection = if (reverseAnimation) {
@@ -25,59 +126,19 @@ open class SlideAnimation(var direction: Direction, private val reverseAnimation
             direction
         }
 
-        if (reverseAnimation) {
-            update = when (actualDirection) {
-                Direction.Up -> { view, proportion ->
-                    val down = view.height * proportion
-
-                    view.clipBounds = Rect(0, 0, view.width, (view.height - down).toInt())
-                    view.translationY = down + initialTranslationY
-                }
-                Direction.Down -> { view, proportion ->
-                    val top = view.height * proportion
-
-                    view.clipBounds = Rect(0, top.toInt(), view.width, view.height)
-                    view.translationY = -top + initialTranslationY
-                }
-                Direction.Left -> { view, proportion ->
-                    val right = view.width * proportion
-
-                    view.clipBounds = Rect(0, 0, (view.width - right).toInt(), view.height)
-                    view.translationX = right + initialTranslationX
-                }
-                Direction.Right -> { view, proportion ->
-                    val left = view.width * proportion
-
-                    view.clipBounds = Rect(left.toInt(), 0, view.width, view.height)
-                    view.translationX = -left + initialTranslationX
-                }
+        slideStrategy = if (reverseAnimation) {
+            when (actualDirection) {
+                Direction.Up -> SlideUpReverseStrategy()
+                Direction.Down -> SlideDownReverseStrategy()
+                Direction.Left -> SlideLeftReverseStrategy()
+                Direction.Right -> SlideRightReverseStrategy()
             }
         } else {
-            update = when (actualDirection) {
-                Direction.Up -> { view, proportion ->
-                    val down = view.height * proportion
-
-                    view.clipBounds = Rect(0, 0, view.width, down.toInt())
-                    view.translationY = view.height - down + initialTranslationY
-                }
-                Direction.Down -> { view, proportion ->
-                    val top = view.height * proportion
-
-                    view.clipBounds = Rect(0, view.height - top.toInt(), view.width, view.height)
-                    view.translationY = -view.height + top + initialTranslationY
-                }
-                Direction.Left -> { view, proportion ->
-                    val right = view.width * proportion
-
-                    view.clipBounds = Rect(0, 0, right.toInt(), view.height)
-                    view.translationX = view.width - right + initialTranslationX
-                }
-                Direction.Right -> { view, proportion ->
-                    val left = view.width * proportion
-
-                    view.clipBounds = Rect(view.width - left.toInt(), 0, view.width, view.height)
-                    view.translationX = -view.width + left + initialTranslationX
-                }
+            when (actualDirection) {
+                Direction.Up -> SlideUpStrategy()
+                Direction.Down -> SlideDownStrategy()
+                Direction.Left -> SlideLeftStrategy()
+                Direction.Right -> SlideRightStrategy()
             }
         }
     }
@@ -88,74 +149,12 @@ open class SlideAnimation(var direction: Direction, private val reverseAnimation
         }
     }
 
+    //TODO create variables for each view
     override fun init(vararg views: View) {
         views.forEach { view -> view.visibility = View.VISIBLE }
 
-        initialTranslationX = views[0].translationX
-        initialTranslationY = views[0].translationY
+        slideStrategy.init(*views)
     }
 
-    override fun updateAnimationByProportion(view: View, proportion: Float) = update(view, proportion)
-
-    override fun runAnimation(view: View, endAction: Runnable?) {
-        view.visibility = View.VISIBLE
-
-        val animator = if (reverseAnimation) {
-            ValueAnimator.ofFloat(0f, 1f)
-        } else {
-            ValueAnimator.ofFloat(1f, 0f)
-        }
-
-        val actualDirection = if (reverseAnimation) {
-            direction.reverse()
-        } else {
-            direction
-        }
-
-        animator.apply {
-            when (actualDirection) {
-                Direction.Up -> addUpdateListener {
-                    val value = this.animatedValue as Float
-                    val down = view.height * value
-
-                    view.clipBounds = Rect(0, 0, view.width, (view.height - down).toInt())
-                    view.translationY = down
-                }
-                Direction.Down -> addUpdateListener {
-                    val value = this.animatedValue as Float
-                    val top = view.height * value
-
-                    view.clipBounds = Rect(0, top.toInt(), view.width, view.height)
-                    view.translationY = -top
-                }
-                Direction.Left -> addUpdateListener {
-                    val value = this.animatedValue as Float
-                    val right = view.width * value
-
-                    view.clipBounds = Rect(0, 0, (view.width - right).toInt(), view.height)
-                    view.translationX = right
-                }
-                Direction.Right -> addUpdateListener {
-                    val value = this.animatedValue as Float
-                    val left = view.width * value
-
-                    view.clipBounds = Rect(left.toInt(), 0, view.width, view.height)
-                    view.translationX = -left
-                }
-            }
-
-            doOnEnd {
-                if (reverseAnimation) {
-                    view.visibility = View.INVISIBLE
-                    view.clipBounds = null
-                    view.translationX = 0.0f
-                    view.translationY = 0.0f
-                }
-                endAction?.run()
-            }
-
-            this.duration = this@SlideAnimation.duration
-        }
-        animator.start()
-    }
+    override fun updateAnimationByProportion(view: View, proportion: Float) = slideStrategy.update(view, proportion)
 }
