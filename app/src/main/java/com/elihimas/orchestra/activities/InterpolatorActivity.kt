35dae4cf36fe4da51b475b3cb181fd24dd3dd184
Arthur Elihimas <arthurelihimas@gmail.com>
2022-07-09 19:23:06 -0300

    Improved interpolator example

--- app/src/main/java/com/elihimas/orchestra/activities/InterpolatorActivity.kt
+++ app/src/main/java/com/elihimas/orchestra/activities/InterpolatorActivity.kt
@@ -20,42 +20,78 @@ class InterpolatorActivity : AppCompatActivity() {
     }
 
     private fun init() {
-        Orchestra.launch {
-            parallel {
-                handleSquare(this, square1, AccelerateDecelerateInterpolator())
-                handleSquare(this, square3, AccelerateInterpolator())
-                handleSquare(this, square2, AnticipateInterpolator())
-                handleSquare(this, square4, BounceInterpolator())
-                handleSquare(this, square5, FastOutLinearInInterpolator())
-                handleSquare(this, square6, OvershootInterpolator())
+        btRun.setOnClickListener { runAnimations(slowly = false) }
+        btRunSlowly.setOnClickListener { runAnimations(slowly = true) }
+        setupSquaresClicks()
+
+        runAnimations(slowly = false)
+    }
+
+    private fun setupSquaresClicks() {
+        val viewsAndInterpolators = listOf(
+            Pair(square1, AccelerateDecelerateInterpolator()),
+            Pair(square2, AccelerateInterpolator()),
+            Pair(square3, AnticipateInterpolator()),
+            Pair(square4, BounceInterpolator()),
+            Pair(square5, FastOutLinearInInterpolator()),
+            Pair(square6, OvershootInterpolator())
+        )
+
+        viewsAndInterpolators.forEach { viewAndInterpolator ->
+            viewAndInterpolator.first.setOnClickListener {
+                Orchestra.launch {
+                    runAnimation(this, viewAndInterpolator.first, viewAndInterpolator.second)
+                }
             }
         }
     }
 
-    private fun handleSquare(orchestraContext: OrchestraContext, view: View, interpolator: Interpolator) {
-        rotate(orchestraContext, view, interpolator)
+    private fun runAnimations(slowly: Boolean) {
+        val viewsAndInterpolators = listOf(
+            Pair(square1, AccelerateDecelerateInterpolator()),
+            Pair(square2, AccelerateInterpolator()),
+            Pair(square3, AnticipateInterpolator()),
+            Pair(square4, BounceInterpolator()),
+            Pair(square5, FastOutLinearInInterpolator()),
+            Pair(square6, OvershootInterpolator())
+        )
 
-        view.setOnClickListener {
+        val durationMultiplier = if (slowly) {
+            4
+        } else {
+            1
+        }
+
+        viewsAndInterpolators.forEach { viewAndInterpolator ->
+            val view = viewAndInterpolator.first
+            val interpolator = viewAndInterpolator.second
+
             Orchestra.launch {
-                rotate(this, view, interpolator)
+                runAnimation(this, view, interpolator, durationMultiplier)
             }
         }
     }
 
-    private fun rotate(orchestraContext: OrchestraContext, view: View, interpolator: Interpolator) {
-        orchestraContext
-                .on(view)
+    private fun runAnimation(
+        orchestraContext: OrchestraContext,
+        view: View,
+        interpolator: Interpolator,
+        durationMultiplier: Int = 1
+    ) {
+        orchestraContext.parallel {
+            on(view)
                 .rotate(180.0f) {
                     this.interpolator = interpolator
-                    duration = 2400
+                    duration = 2400L * durationMultiplier
                 }
-                .scale(2){
+                .scale(2) {
                     this.interpolator = interpolator
-                    duration = 2000
+                    duration = 2000L * durationMultiplier
                 }
-                .scale(1){
+                .scale(1) {
                     this.interpolator = interpolator
-                    duration = 2000
+                    duration = 2000L * durationMultiplier
                 }
+        }
     }
 }
